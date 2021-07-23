e8cd72960977ec36ff67f336e1a515aaaae782ff
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-07-23 14:44:13 +0530

    checkpoint

--- block-BNaaft/demo/events/routes/category.js
+++ block-BNaaft/demo/events/routes/category.js
@@ -4,15 +4,21 @@ var Event = require('../models/events');
 var Remark = require('../models/remarks');
 /* GET home page. */
 
+// router.get('/:username', function (req, res, next) {
+//   var name = req.params.username;
+//   Event.find({}).exec((err, events) => {
+//     var arrayEvents = events.filter((event) => {
+//       if (event.event_category.split(',').includes(name)) {
+//         return event;
+//       }
+//     });
+//     res.render('category', { events: arrayEvents });
+//   });
+// });
 router.get('/:username', function (req, res, next) {
   var name = req.params.username;
-  Event.find({}).exec((err, events) => {
-    var arrayEvents = events.filter((event) => {
-      if (event.event_category.split(',').includes(name)) {
-        return event;
-      }
-    });
-    res.render('category', { events: arrayEvents });
+  Event.find({ event_category: name }).exec((err, events) => {
+    res.render('category', { events: events });
   });
 });
 module.exports = router;
